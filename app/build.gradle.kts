b1177fcc3cab11ec777958f35b580224ec9dc0dd
Andy Clarke <leatfield@gmail.com>
2026-07-28 17:15:41 +0100

    chore(release): bump version to 27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.humanstrength.kfqjza"
     minSdk = 24
     targetSdk = 36
-    versionCode = 26
-    versionName = "26.0"
+    versionCode = 27
+    versionName = "27.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
