dd71cebe257b849c51125e70f4b36deaa27b42ff
Andy Clarke <leatfield@gmail.com>
2026-08-21 16:17:29 +0100

    chore(release): prepare Strength version 38

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.humanstrength.kfqjza"
     minSdk = 24
     targetSdk = 36
-    versionCode = 37
-    versionName = "37.0"
+    versionCode = 38
+    versionName = "38.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
