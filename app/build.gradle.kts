d4e8a3397a6029e46c0ab46a418ae1ff2d4fcf18
Andy Clarke <leatfield@gmail.com>
2026-09-14 13:05:30 +0100

    chore: prepare Android version 42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.humanstrength.kfqjza"
     minSdk = 24
     targetSdk = 36
-    versionCode = 41
-    versionName = "41.0"
+    versionCode = 42
+    versionName = "42.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
